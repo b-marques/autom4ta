9f02d0adb572e4962228c4dda9b0a483d17319fe
Bruno Marques <brunomn95@gmail.com>
2018-10-16 03:58:06 -0300

    Updates

---
+++ precache-manifest.5d2927134ed202a30d1e46477c556fa8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad3d725a13480c7530d833e681b80148",
+    "url": "/autom4ta/static/media/logo.ad3d725a.svg"
+  },
+  {
+    "revision": "19bdbd092c06b2a20ab1",
+    "url": "/autom4ta/static/js/runtime~main.19bdbd09.js"
+  },
+  {
+    "revision": "0835587029363c27aebe",
+    "url": "/autom4ta/static/js/main.08355870.chunk.js"
+  },
+  {
+    "revision": "99dd77fbc10ab672d181",
+    "url": "/autom4ta/static/js/1.99dd77fb.chunk.js"
+  },
+  {
+    "revision": "0835587029363c27aebe",
+    "url": "/autom4ta/static/css/main.5071d05b.chunk.css"
+  },
+  {
+    "revision": "1b1fa727fe014b68dc985616685c16f9",
+    "url": "/autom4ta/index.html"
+  }
+];
